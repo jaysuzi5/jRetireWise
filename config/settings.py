140ba180c66fafcb74dd661645ef4c73807fffe2
Jay Curtis <jaysuzi5@gmail.com>
2025-12-04 23:17:46 -0500

    fix: Add WhiteNoise configuration to enable proper static file serving

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
+# WhiteNoise Configuration
+WHITENOISE_AUTOREFRESH = True
+WHITENOISE_USE_FINDERS = True
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
